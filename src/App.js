82793e4f7bbee0f213c2ac3bd394c2b1a47f675f
adityaraj309050 <adityaraj309050@gmail.com>
2026-05-24 17:21:08 +0530

    01

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import './index.css';
